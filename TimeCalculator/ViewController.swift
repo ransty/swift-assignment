524d65119353eaced17c8c02d3754020a8185f7d
Porcaro, Keano Dean - porkd002 <porkd002@f120-06.local>
2017-09-07 13:48:36 +0930

    Initial Commit

---
+++ TimeCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeCalculator
+//
+//  Created by Porcaro, Keano Dean - porkd002 on 7/9/17.
+//  Copyright © 2017 Porcaro, Keano Dean - porkd002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
